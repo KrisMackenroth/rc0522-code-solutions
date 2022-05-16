43d2ccb37476dfe1028a94f3d3336fbbdb293d88
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-16 17:10:32 +0000

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,29 @@
+var fullName = 'Kristopher Erik Mackenroth';
+
+var isCool = true;
+
+var totalPets = 2;
+
+console.log(fullName);
+
+console.log(isCool);
+
+console.log(totalPets);
+
+console.log(typeof fullName);
+
+console.log(typeof isCool);
+
+console.log(typeof totalPets);
+
+var unknown = null;
+
+console.log(unknown);
+
+console.log(typeof unknown);
+
+var never;
+
+console.log(never);
+
+console.log(typeof never);
