4e2f30281724f27ff59e1177f3db4e1695761c79
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-16 18:19:16 +0000

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,65 @@
+var width = 4;
+
+var height = 5;
+
+var area = width * height;
+
+console.log(area);
+
+console.log(typeof area);
+
+var bill = 10;
+
+var payment = 20;
+
+var change = payment - bill;
+
+console.log(change);
+
+console.log(typeof change);
+
+var quizzes = 10;
+
+var midterm = 20;
+
+var final = 30;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log(grade);
+
+console.log(typeof grade);
+
+var firstName = 'Kris';
+
+var lastName = 'Mackenroth';
+
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+console.log(typeof fullName);
+
+var pH = 15;
+
+var isAcidic = pH > 7;
+
+console.log(isAcidic);
+
+console.log(typeof isAcidic);
+
+var headCount = 16;
+
+var isSparta = headCount === 300;
+
+console.log(isSparta);
+
+console.log(typeof isSparta);
+
+var motto = fullName;
+
+motto = motto + ' is the Goat';
+
+console.log(motto);
+
+console.log(typeof motto);
