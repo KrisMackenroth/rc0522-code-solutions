f0426525409c1bad09be0ca80a83b0bcf7f919ef
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-17 17:19:53 +0000

    Completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,40 @@
+var orderHistory = [
+  {
+    ID: '114-3941689-8772232',
+    Type: 'Book',
+    Name: 'JavaScript for impatient programmers',
+    Date: 'Aug 8, 2020',
+    Price: '$31.55'
+  },
+  {
+    ID: '113-9984268-1280257',
+    Type: 'Book',
+    Name: 'The Timeless Way of Building',
+    Date: 'Jul 20, 2020',
+    Price: '$41.33'
+
+  },
+  {
+    ID: '114-2875557-9059409',
+    Type: 'Accessory',
+    Name: 'Gamecube Controller Adapter',
+    Date: 'Jul 7, 2020',
+    Price: '$15.98'
+  },
+  {
+    ID: '113-2883177-2648248',
+    Type: 'Accessory',
+    Name: 'GameCube Controller - Super Smash Bros',
+    Date: 'Jul 5, 2020',
+    Price: '$94.95'
+  },
+  {
+    ID: '113-2883177-2648248',
+    Type: 'Book',
+    Name: 'The Art of Sql',
+    Date: 'Jul 5, 2020',
+    Price: '$33.99'
+  }
+];
+
+console.log(orderHistory[1].ID);
