dcf5c98f37c8c4c13d0dd5c58e55128e6dfa760e
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-24 04:37:17 +0000

    Progress.

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,10 +1,12 @@
 /* exported equal */
 function equal(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
   for (var i = 0; i < first.length; i++) {
-    for (var j = 0; j < second.length; j++) {
-      if (first[i] !== second[j]) {
-        return false;
-      }
+    if (first[i] !== second[i]) {
+      return false;
     }
-  } return true;
+  }
+  return true;
 }
