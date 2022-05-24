a0fc2d19b7b6947d5fe7342af27f35018e40bb14
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-24 17:21:22 +0000

    Progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -4,7 +4,7 @@ function titleCase(title) {
   var lowerCase = title.toLowerCase();
   var split = lowerCase.split(' ');
   for (var i = 0; i < split.length; i++) {
-    if (split[i].length >= 3 || split[i] !== 'for') {
+    if (split[i] !== 'in') {
       split[i] = split[i][0].toUpperCase() + split[i].slice(1);
     }
   }
