28a91de9ecda25170212fcfe698d71ed6baffa32
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-25 21:41:27 +0000

    Progress.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,21 +1,62 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-// var playerOne = { name: 'bob', hand: [] };
-// var playerTwo = { name: 'cole', hand: [] };
-// var playerThree = { name: 'daniel', hand: [] };
-// var playerFour = { name: 'greg', hand: [] };
+var playerOne = { name: 'bob', hand: {} };
+var playerTwo = { name: 'cole', hand: [] };
+var playerThree = { name: 'daniel', hand: [] };
+var playerFour = { name: 'greg', hand: [] };
 
-// var deck = { suits: ['Hearts', 'Spades', 'Clubs', 'Diamonds'], values: ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'] };
+const suits = ['Spades', 'Diamonds', 'Club', 'Heart'];
+const values = [
+  'Ace',
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  'Jack',
+  'Queen',
+  'King'
+];
 
-// // shuffle
+// shuffle
 
-// var suits = deck.suits;
-// var values = deck.values;
-// var combined = [];
-// console.log(suits);
+var combined = [];
 
-// for (var i = 0; i < suits.length; i++) {
-//   for (var j = 0; j < values.length; j++) {
-//     combined.push(suits[i], values[j]);
-//   }
-// }
-// console.log(combined);
+for (var i = 0; i < suits.length; i++) {
+  for (var j = 0; j < values.length; j++) {
+    var card = { Value: values[j], Suit: suits[i] };
+    combined.push(card);
+  }
+}
+
+for (var k = combined.length - 1; k > 0; k--) {
+  var x = Math.floor(Math.random() * k);
+  var temp = combined[k];
+  combined[k] = combined[x];
+  combined[x] = temp;
+
+}
+
+playerOne.hand[0] = combined[x];
+playerOne.hand[1] = combined[x + 1];
+playerTwo.hand[0] = combined[x + 2];
+playerTwo.hand[1] = combined[x + 3];
+playerThree.hand[0] = combined[x + 4];
+playerThree.hand[1] = combined[x + 5];
+playerFour.hand[0] = combined[x + 6];
+playerFour.hand[1] = combined[x + 7];
+
+if (playerOne.hand.value === 'Ace') {
+  playerOne.hand.value = 11;
+}
+
+if (playerOne.hand.value === 'King' || playerOne.hand.value === 'Queen' || playerOne.hand.value === 'Jack') {
+  playerOne.hand.value = 10;
+}
+console.log(playerOne);
+console.log(playerTwo);
+console.log(playerThree);
+console.log(playerFour);
