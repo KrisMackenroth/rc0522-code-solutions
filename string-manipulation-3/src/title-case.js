cad4a202e52ab4a391e93bdd2a33ae064572a5b6
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-25 00:56:16 +0000

    progres..

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -4,7 +4,11 @@ function titleCase(title) {
   var lowerCase = title.toLowerCase();
   var split = lowerCase.split(' ');
   for (var i = 0; i < split.length; i++) {
-    if (split[i] !== 'in') {
+    if (split[i] === 'javascript') {
+      split[i] = 'JavaScript';
+    } else if (split[i] === 'api') {
+      split[i] = 'API';
+    } else if (split[i] !== 'in' && split[i] !== 'for' && split[i] !== 'of' && split[i] !== 'and') {
       split[i] = split[i][0].toUpperCase() + split[i].slice(1);
     }
   }
