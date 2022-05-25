03138d534b1228a32af3879446268713ce86e049
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-25 19:19:24 +0000

    Progress.

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,21 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// var playerOne = { name: 'bob', hand: [] };
+// var playerTwo = { name: 'cole', hand: [] };
+// var playerThree = { name: 'daniel', hand: [] };
+// var playerFour = { name: 'greg', hand: [] };
+
+// var deck = { suits: ['Hearts', 'Spades', 'Clubs', 'Diamonds'], values: ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'] };
+
+// // shuffle
+
+// var suits = deck.suits;
+// var values = deck.values;
+// var combined = [];
+// console.log(suits);
+
+// for (var i = 0; i < suits.length; i++) {
+//   for (var j = 0; j < values.length; j++) {
+//     combined.push(suits[i], values[j]);
+//   }
+// }
+// console.log(combined);
