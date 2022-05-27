585ce6f7456b9e93a9c8bea178992f530e4be7fd
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-27 22:47:32 +0000

    Added user input.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,11 +1,28 @@
-var spanial = document.querySelectorAll('span');
-var temp = 0;
+var button = document.getElementById('myButton');
 
-document.addEventListener('keydown', function (e) {
-  var keys = e.key;
-  if (keys === spanial[temp].textContent) {
-    spanial[temp].className = 'green-text';
-    spanial[temp + 1].classList.add('under-border');
-    temp++;
-  } else { spanial[temp].className = 'red-text'; }
+button.addEventListener('click', function (event) {
+  var input = document.getElementById('userInput').value;
+  var splitInput = input.split('');
+  var main = document.querySelector('main');
+  var i = 0;
+  do {
+    var spanTest = document.createElement('span');
+    var wordContent = document.createTextNode(splitInput[i]);
+    spanTest.appendChild(wordContent);
+    main.appendChild(spanTest);
+    i++;
+  } while (i < splitInput.length);
+
+  var spanial = document.querySelectorAll('span');
+  var temp = 0;
+
+  document.addEventListener('keydown', function (e) {
+    var keys = e.key;
+    if (keys === spanial[temp].textContent) {
+      spanial[temp].className = 'green-text';
+      spanial[temp + 1].classList.add('under-border');
+      temp++;
+    } else { spanial[temp].className = 'red-text'; }
+  });
+
 });
