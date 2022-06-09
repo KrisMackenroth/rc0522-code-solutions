043bfa424307a4a99ce4af137fde1b27ce7232c3
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-06-09 00:53:20 +0000

    Updated.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -21,43 +21,24 @@ var count = 1;
 function countDecrease() {
   if (count === 0) {
     image.setAttribute('src', 'images/001.png');
-    one.classList.remove('background-white');
     one.classList.add('background-black');
     five.classList.remove('background-black');
-    five.classList.add('background-white');
   } else if (count === 1) {
     image.setAttribute('src', 'images/004.png');
     one.classList.remove('background-black');
-    one.classList.add('background-white');
-    two.classList.remove('background-white');
     two.classList.add('background-black');
   } else if (count === 2) {
     image.setAttribute('src', 'images/007.png');
     two.classList.remove('background-black');
-    two.classList.add('background-white');
-    three.classList.remove('background-white');
     three.classList.add('background-black');
   } else if (count === 3) {
     image.setAttribute('src', 'images/025.png');
     three.classList.remove('background-black');
-    three.classList.add('background-white');
-    four.classList.remove('background-white');
     four.classList.add('background-black');
   } else if (count === 4) {
     image.setAttribute('src', 'images/039.png');
     four.classList.remove('background-black');
-    four.classList.add('background-white');
-    five.classList.remove('background-white');
     five.classList.add('background-black');
-  } else if (count === 5) {
-    count = 0;
-    image.setAttribute('src', 'images/001.png');
-    one.classList.remove('background-white');
-    one.classList.add('background-black');
-    five.classList.remove('background-black');
-    five.classList.add('background-white');
-    count++;
-    return count;
   }
   count++;
   return count;
@@ -68,15 +49,10 @@ var newInterval = setInterval(countDecrease, 3000);
 buttons.addEventListener('click', function (event) {
   clearInterval(newInterval);
   two.classList.remove('background-black');
-  two.classList.add('background-white');
   one.classList.remove('background-black');
-  one.classList.add('background-white');
   three.classList.remove('background-black');
-  three.classList.add('background-white');
   four.classList.remove('background-black');
-  four.classList.add('background-white');
   five.classList.remove('background-black');
-  five.classList.add('background-white');
   if (event.target.classList.contains('one')) {
     count = 0;
   } else if (event.target.classList.contains('two')) {
@@ -97,16 +73,12 @@ backward.addEventListener('click', function (event) {
   clearInterval(newInterval);
   count = count - 2;
   if (count === 0) {
-    two.classList.add('background-white');
     two.classList.remove('background-black');
   } else if (count === 1) {
-    three.classList.add('background-white');
     three.classList.remove('background-black');
   } else if (count === 2) {
-    four.classList.add('background-white');
     four.classList.remove('background-black');
   } else if (count === 3) {
-    five.classList.add('background-white');
     five.classList.remove('background-black');
   }
   countDecrease();
@@ -115,7 +87,6 @@ backward.addEventListener('click', function (event) {
 });
 
 forward.addEventListener('click', function (event) {
-
   clearInterval(newInterval);
   countDecrease();
   newInterval = setInterval(countDecrease, 3000);
