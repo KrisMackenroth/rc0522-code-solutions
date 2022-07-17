8268e098044b1258b3dd03936a8c57b17db22052
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-07-17 14:58:50 +0000

    Completed react-accordion-component.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 
+const topics = [
+  { description: 'jdfksadflksajdfjdfksadflksajdfjdfksadflksajdf', name: 'Bulbasaur' },
+  { description: 'jdfksadflksajdfjdfksadflksajdfjdfksadflksajdf', name: 'Charmander' },
+  { description: 'jdfksadflksajdfjdfksadflksajdfjdfksadflksajdf', name: 'Squirtle' },
+  { description: 'jdfksadflksajdfjdfksadflksajdfjdfksadflksajdf', name: 'Pikachu' },
+  { description: 'jdfksadflksajdfjdfksadflksajdfjdfksadflksajdf', name: 'Jigglypuff' }
+];
+
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
+    this.state = { isClicked: 'false' };
     this.handleClick = this.handleClick.bind(this);
-
   }
 
   handleClick(event) {
     if (event.target.className === 'closed') {
-      event.target.className = 'open';
+      for (let x = 0; x < event.target.parentElement.parentElement.children.length; x++) {
+        event.target.parentElement.parentElement.children[x].children[0].className = 'closed';
+        event.target.parentElement.parentElement.children[x].children[1].className = 'hidden';
+        event.target.className = 'open';
+        event.target.nextElementSibling.className = 'visible';
+      }
     } else {
       event.target.className = 'closed';
+      event.target.nextElementSibling.className = 'hidden';
     }
   }
 
   render() {
-    const listItems = topics.map(topic => <li className='closed' onClick={this.handleClick} key={topic.name}>{topic.name}</li>);
-
+    const listItems = topics.map(topic => <div key={topic.name}><li className='closed' onClick={this.handleClick} key={topic.name}>{topic.name}</li><li className='hidden'>{topic.description}</li></div>);
     return (
       <ul>
         {listItems}
@@ -26,11 +38,3 @@ export default class Accordion extends React.Component {
     );
   }
 }
-
-const topics = [
-  { description: 'jdfksadflksajdf', name: 'Bulbasaur' },
-  { description: 'jdfksadflksajdf', name: 'Charmander' },
-  { description: 'jdfksadflksajdf', name: 'Squirtle' },
-  { description: 'jdfksadflksajdf', name: 'Pikachu' },
-  { description: 'jdfksadflksajdf', name: 'Jigglypuff' }
-];
